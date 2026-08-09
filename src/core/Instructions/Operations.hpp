5c0dbd9727c0159e85df120c15b366612706ed34
KovarJoel <joel.m.kovar@gmail.com>
2026-08-09 10:55:49 +0200

    added operand count and case insensitive comparison

--- src/core/Instructions/Operations.hpp
+++ src/core/Instructions/Operations.hpp
@@ -2,46 +2,47 @@
 
 #include "core/Exceptions.hpp"
 
+#include <cctype>
 #include <cstdint>
 #include <string_view>
 
-// mnemonic, operation type, operation class, init
+// mnemonic, operation type, operation class, dest operands count, src operands count, init
 #define EMULATOR_CORE_INSTRUCTIONS_OPCODE_LIST(X) \
-  X(LD,     OpcodeType::Memory,       Load,                       =1)\
-  X(LDZX,   OpcodeType::Memory,       LoadZeroExtend,               )\
-  X(ST,     OpcodeType::Memory,       Store,                        )\
-  X(ADD,    OpcodeType::Arithmetic,   Add,                          )\
-  X(SUB,    OpcodeType::Arithmetic,   Subtract,                     )\
-  X(MUL,    OpcodeType::Arithmetic,   Multiply,                     )\
-  X(DIV,    OpcodeType::Arithmetic,   Divide,                       )\
-  X(DIVU,   OpcodeType::Arithmetic,   DivideUnsigned,               )\
-  X(REM,    OpcodeType::Arithmetic,   Remainder,                    )\
-  X(REMU,   OpcodeType::Arithmetic,   RemainderUnsigned,            )\
-  X(ROL,    OpcodeType::Rotate,       RotateLeft,                   )\
-  X(ROR,    OpcodeType::Rotate,       RotateRight,                  )\
-  X(SLL,    OpcodeType::Shift,        ShiftLeftLogical,             )\
-  X(SRL,    OpcodeType::Shift,        ShiftRightLogical,            )\
-  X(SRA,    OpcodeType::Shift,        ShiftRightArithmetic,         )\
-  X(AND,    OpcodeType::Logical,      And,                          )\
-  X(OR,     OpcodeType::Logical,      Or,                           )\
-  X(XOR,    OpcodeType::Logical,      Xor,                          )\
-  X(JMP,    OpcodeType::Jump,         Jump,                         )\
-  X(BE,     OpcodeType::Branch,       BranchEqual,                  )\
-  X(BNE,    OpcodeType::Branch,       BranchNotEqual,               )\
-  X(BGT,    OpcodeType::Branch,       BranchGreater,                )\
-  X(BLT,    OpcodeType::Branch,       BranchLess,                   )\
-  X(BGE,    OpcodeType::Branch,       BranchGreaterEqual,           )\
-  X(BLE,    OpcodeType::Branch,       BranchLessEqual,              )\
-  X(BGTU,   OpcodeType::Branch,       BranchGreaterUnsigned,        )\
-  X(BLTU,   OpcodeType::Branch,       BranchLessUnsigned,           )\
-  X(BGEU,   OpcodeType::Branch,       BranchGreaterEqualUnsigned,   )\
-  X(BLEU,   OpcodeType::Branch,       BranchLessEqualUnsigned,      )\
-  X(BC,     OpcodeType::Branch,       BranchCarry,                  )\
-  X(BNC,    OpcodeType::Branch,       BranchNotCarry,               )\
-  X(BO,     OpcodeType::Branch,       BranchOverflow,               )\
-  X(BNO,    OpcodeType::Branch,       BranchNotOverflow,            )\
-  X(HALT,   OpcodeType::Other,        Halt,                         )\
-  X(CYCL,   OpcodeType::Other,        Cycle,                        )
+  X(LD,     OpcodeType::Memory,       Load,                       1,  2,  =1)\
+  X(LDZX,   OpcodeType::Memory,       LoadZeroExtend,             1,  2,    )\
+  X(ST,     OpcodeType::Memory,       Store,                      2,  1,    )\
+  X(ADD,    OpcodeType::Arithmetic,   Add,                        1,  2,    )\
+  X(SUB,    OpcodeType::Arithmetic,   Subtract,                   1,  2,    )\
+  X(MUL,    OpcodeType::Arithmetic,   Multiply,                   1,  2,    )\
+  X(DIV,    OpcodeType::Arithmetic,   Divide,                     1,  2,    )\
+  X(DIVU,   OpcodeType::Arithmetic,   DivideUnsigned,             1,  2,    )\
+  X(REM,    OpcodeType::Arithmetic,   Remainder,                  1,  2,    )\
+  X(REMU,   OpcodeType::Arithmetic,   RemainderUnsigned,          1,  2,    )\
+  X(ROL,    OpcodeType::Rotate,       RotateLeft,                 1,  2,    )\
+  X(ROR,    OpcodeType::Rotate,       RotateRight,                1,  2,    )\
+  X(SLL,    OpcodeType::Shift,        ShiftLeftLogical,           1,  2,    )\
+  X(SRL,    OpcodeType::Shift,        ShiftRightLogical,          1,  2,    )\
+  X(SRA,    OpcodeType::Shift,        ShiftRightArithmetic,       1,  2,    )\
+  X(AND,    OpcodeType::Logical,      And,                        1,  2,    )\
+  X(OR,     OpcodeType::Logical,      Or,                         1,  2,    )\
+  X(XOR,    OpcodeType::Logical,      Xor,                        1,  2,    )\
+  X(JMP,    OpcodeType::Jump,         Jump,                       0,  1,    )\
+  X(BE,     OpcodeType::Branch,       BranchEqual,                0,  1,    )\
+  X(BNE,    OpcodeType::Branch,       BranchNotEqual,             0,  1,    )\
+  X(BGT,    OpcodeType::Branch,       BranchGreater,              0,  1,    )\
+  X(BLT,    OpcodeType::Branch,       BranchLess,                 0,  1,    )\
+  X(BGE,    OpcodeType::Branch,       BranchGreaterEqual,         0,  1,    )\
+  X(BLE,    OpcodeType::Branch,       BranchLessEqual,            0,  1,    )\
+  X(BGTU,   OpcodeType::Branch,       BranchGreaterUnsigned,      0,  1,    )\
+  X(BLTU,   OpcodeType::Branch,       BranchLessUnsigned,         0,  1,    )\
+  X(BGEU,   OpcodeType::Branch,       BranchGreaterEqualUnsigned, 0,  1,    )\
+  X(BLEU,   OpcodeType::Branch,       BranchLessEqualUnsigned,    0,  1,    )\
+  X(BC,     OpcodeType::Branch,       BranchCarry,                0,  1,    )\
+  X(BNC,    OpcodeType::Branch,       BranchNotCarry,             0,  1,    )\
+  X(BO,     OpcodeType::Branch,       BranchOverflow,             0,  1,    )\
+  X(BNO,    OpcodeType::Branch,       BranchNotOverflow,          0,  1,    )\
+  X(HALT,   OpcodeType::Other,        Halt,                       0,  0,    )\
+  X(CYCL,   OpcodeType::Other,        Cycle,                      1,  0,    )
 
 namespace core::instructions {
   enum class OpcodeType {
@@ -56,7 +57,7 @@ namespace core::instructions {
   };
 
   enum class Opcode : uint8_t {
-    #define EMULATOR_CORE_INSTRUCTIONS_OPCODE_LIST_ENUM(mnemonic, type, class, init) mnemonic init,
+    #define EMULATOR_CORE_INSTRUCTIONS_OPCODE_LIST_ENUM(mnemonic, type, class, dest, src, init) mnemonic init,
       EMULATOR_CORE_INSTRUCTIONS_OPCODE_LIST(EMULATOR_CORE_INSTRUCTIONS_OPCODE_LIST_ENUM)
     #undef EMULATOR_CORE_INSTRUCTIONS_OPCODE_LIST_ENUM
   };
@@ -64,6 +65,24 @@ namespace core::instructions {
   constexpr OpcodeType opcodeGetType(Opcode code) {
     switch (code) {
       #define EMULATOR_CORE_INSTRUCTIONS_OPCODE_LIST_CASE(mnemonic, type, ...) case Opcode::mnemonic: return type;
+        EMULATOR_CORE_INSTRUCTIONS_OPCODE_LIST(EMULATOR_CORE_INSTRUCTIONS_OPCODE_LIST_CASE)
+      #undef EMULATOR_CORE_INSTRUCTIONS_OPCODE_LIST_CASE
+    }
+    throw exceptions::InvalidOperation{};
+  }
+
+  constexpr size_t opcodeGetDestCount(Opcode code) {
+    switch (code) {
+      #define EMULATOR_CORE_INSTRUCTIONS_OPCODE_LIST_CASE(mnemonic, type, class, dest, ...) case Opcode::mnemonic: return dest;
+        EMULATOR_CORE_INSTRUCTIONS_OPCODE_LIST(EMULATOR_CORE_INSTRUCTIONS_OPCODE_LIST_CASE)
+      #undef EMULATOR_CORE_INSTRUCTIONS_OPCODE_LIST_CASE
+    }
+    throw exceptions::InvalidOperation{};
+  }  
+  
+  constexpr size_t opcodeGetSrcCount(Opcode code) {
+    switch (code) {
+      #define EMULATOR_CORE_INSTRUCTIONS_OPCODE_LIST_CASE(mnemonic, type, class, dest, src, ...) case Opcode::mnemonic: return src;
         EMULATOR_CORE_INSTRUCTIONS_OPCODE_LIST(EMULATOR_CORE_INSTRUCTIONS_OPCODE_LIST_CASE)
       #undef EMULATOR_CORE_INSTRUCTIONS_OPCODE_LIST_CASE
     }
@@ -89,7 +108,17 @@ namespace core::instructions {
   }
 
   constexpr Opcode opcodeFromString(std::string_view string) {
-    #define EMULATOR_CORE_INSTRUCTIONS_OPCODE_LIST_IF(mnemonic, ...) if (string == #mnemonic) return Opcode::mnemonic;
+    const auto equals = [](std::string_view lhs, std::string_view rhs) {
+      if (lhs.size() != rhs.size()) return false;
+      for (size_t i = 0; i < lhs.size(); ++i) {
+        if (std::tolower(lhs[i]) != std::tolower(rhs[i])) {
+          return false;
+        }
+      }
+      return true;
+    };
+
+    #define EMULATOR_CORE_INSTRUCTIONS_OPCODE_LIST_IF(mnemonic, ...) if (equals(string, #mnemonic)) return Opcode::mnemonic;
       EMULATOR_CORE_INSTRUCTIONS_OPCODE_LIST(EMULATOR_CORE_INSTRUCTIONS_OPCODE_LIST_IF)
     #undef EMULATOR_CORE_INSTRUCTIONS_OPCODE_LIST_IF
     throw exceptions::InvalidOperation{};
